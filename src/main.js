f55833f7b5b058bbc353ac330e54583c350418ab
mark <markx931@gmail.com>
2023-06-30 18:36:51 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import LoginRegisterModal from './components/LoginSignup/LoginRegisterModal.vue'
+//import LoginRegisterModal from './components/LoginSignup/LoginRegisterModal.vue'
 import bootstrap from 'bootstrap/dist/js/bootstrap.js'
 import './assets/main.css'
 
